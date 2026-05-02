a9836c8251d3f392eb8e20e172e5932d55260c8d
Anish-Kumar-Singh <150132644+Anish-Kumar-Singh@users.noreply.github.com>
2026-05-02 22:37:26 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,29 +6,20 @@ const path = require("path");
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
+// routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/projects", require("./routes/projects"));
 app.use("/api/tasks", require("./routes/tasks"));
 
-// ================= FRONTEND (IMPORTANT FIX) =================
-
-// This works when structure is:
-// team-task-manager/
-//   server/
-//   client/
-
-app.use(express.static(path.join(__dirname, "../client")));
+// ✅ now client is inside server
+app.use(express.static(path.join(__dirname, "client")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/index.html"));
+  res.sendFile(path.join(__dirname, "client", "index.html"));
 });
-
-// ================= SERVER =================
 
 const PORT = process.env.PORT || 5000;
 
