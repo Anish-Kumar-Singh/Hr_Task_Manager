16351d2c1d8ffaf4a30595b75cc70508ace4db86
Anish-Kumar-Singh <150132644+Anish-Kumar-Singh@users.noreply.github.com>
2026-05-02 22:06:11 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,19 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+// routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/projects", require("./routes/projects"));
 app.use("/api/tasks", require("./routes/tasks"));
-const path = require("path");
 
-app.use(express.static(path.join(__dirname, "../client")));
+// ✅ FIXED FRONTEND PATH
+app.use(express.static(path.join(__dirname, "client")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/index.html"));
+  res.sendFile(path.join(__dirname, "client", "index.html"));
 });
+
 app.listen(process.env.PORT, () => console.log("Server running"));
