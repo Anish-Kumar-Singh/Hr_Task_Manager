ced9af594f4230f47bdf1eff3bfa828632756182
Anish-Kumar-Singh <150132644+Anish-Kumar-Singh@users.noreply.github.com>
2026-05-02 18:30:46 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,5 +9,11 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/projects", require("./routes/projects"));
 app.use("/api/tasks", require("./routes/tasks"));
+const path = require("path");
 
+app.use(express.static(path.join(__dirname, "../client")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/index.html"));
+});
 app.listen(process.env.PORT, () => console.log("Server running"));
